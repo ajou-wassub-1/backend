2f190c926b469aafa355061be7dbd1ec0731bd2d
lhj <sqaurelu@ajou.ac.kr>
2021-01-12 10:05:52 +0900

    test

---
+++ db.js
@@ -0,0 +1,11 @@
+// const mysql = require('mysql');
+// const pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: 'mysql',
+//     user: 'root',
+//     password: 'sqaurelu',
+//     database: 'myapp'
+// });
+
+// // export default pool;
+// exports.pool = pool;
